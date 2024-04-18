6ef2d59b9f91480b6bd098c33ed88573ee0e8877
Win Naing Soe <winnaingsoe@agbcommunication.com>
2024-04-18 07:50:12 +0630

    project init

---
+++ agb-lab-application/src/main/java/com/agb/lab/application/AgbLabApplication.java
@@ -0,0 +1,11 @@
+package com.agb.lab.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgbLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgbLabApplication.class, args);
+    }
+}
